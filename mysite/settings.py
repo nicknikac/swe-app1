8b4e4cf0279f37d728381bebb019e597bcb4538c
nicknikac <nnikac11@gmail.com>
2025-10-30 00:03:28 -0400

    Fix Procfile to use WSGI application object and update STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "/var/app/current/staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
